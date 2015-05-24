4b721c6904073a4ca36a9edc306ab34a1b6cc72c
Dentosal <hannes.karppila@gmail.com>
2015-05-24 14:32:37 +0300

    Add script "class.py", fix missing startroom

---
+++ class.py
@@ -0,0 +1,8 @@
+def onenter(api, variables, inventory, items):
+
+	return variables, inventory, items
+
+
+def oncommand(api, command, variables, inventory, items):
+
+	return False, variables, inventory, items
